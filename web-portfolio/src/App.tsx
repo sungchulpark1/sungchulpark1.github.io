f13f9bf355573b779dc82d05f1d72321e9bc90a9
Sungchul Park <spark0843@gmail.com>
2025-08-01 03:47:48 -0400

    feat: add initial screen

--- web-portfolio/src/App.tsx
+++ web-portfolio/src/App.tsx
@@ -1,4 +1,5 @@
 import { motion } from "motion/react"
+import { FaChevronCircleDown } from "react-icons/fa"
 
 import './App.css'
 
@@ -6,13 +7,32 @@ function App() {
   return (
     <>
       <motion.div
-        className="logo"
+        // className="logo"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5, ease: [0, 0, 0.4, 1] }}
       >
         <h1>Sungchul Park</h1>
         <h2>Full Stack Developer</h2>
+      </motion.div>
+
+      <motion.div
+        // className="description"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 20 }}
+        transition={{ duration: 0.5, delay: 0.3, ease: [0, 0, 0.4, 1] }}
+      >
+        <p>
+          Hello! I'm a Full Stack Developer based in Mississauga, Ontario, Canada.
+        </p>
+
+        <p>
+          I specialize in building web applications using React, Node.js, and various other technologies.
+        </p>
+
+        <div className="scroll-down">
+          <FaChevronCircleDown size={32} />
+        </div>
       </motion.div>
     </>
   )

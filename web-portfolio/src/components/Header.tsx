fb11567b3f939cf7cdde7a3de39de26d17ababea
Sungchul Park <spark0843@gmail.com>
2025-09-04 21:21:38 -0400

    improve framer motion, add link to avatar leading to linkedin profile

--- web-portfolio/src/components/Header.tsx
+++ web-portfolio/src/components/Header.tsx
@@ -3,35 +3,37 @@ import avatar from "./../assets/avatar.png"
 
 function Header() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="min-h-[calc(100vh-theme(spacing.8)*2)] flex flex-col justify-center items-center p-8">
       <motion.div
-        className="flex flex-col justify-center items-center w-full text-center"
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5, ease: [0, 0, 0.4, 1] }}
+      className="flex flex-col justify-center items-center w-full text-center"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5, ease: [0, 0, 0.4, 1] }}
       >
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Sungchul Park
-        </h1>
-        <h2 className="text-xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl dark:text-white">
-          Full Stack Developer
-        </h2>
-        <div className="mt-6 mb-2">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Sungchul Park
+      </h1>
+      <h2 className="text-xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl dark:text-white">
+        Full Stack Developer
+      </h2>
+      <div className="mt-6 mb-2">
+        <a href="https://www.linkedin.com/in/sungchulpark1/">
           <img src={avatar} alt="Sungchul Park" className="w-32 h-32 rounded-full" />
-        </div>
+        </a>
+      </div>
       </motion.div>
       <motion.div
-        className="flex flex-col justify-center items-center w-4/5 mb-8 text-center"
-        initial={{ opacity: 0, y: 0 }}
-        animate={{ opacity: 1, y: 20 }}
-        transition={{ duration: 0.5, delay: 0.3, ease: [0, 0, 0.4, 1] }}
+      className="flex flex-col justify-center items-center w-4/5 mb-8 text-center"
+      initial={{ opacity: 0, y: 0 }}
+      animate={{ opacity: 1, y: 20 }}
+      transition={{ duration: 0.5, delay: 0.3, ease: [0, 0, 0.4, 1] }}
       >
-        <p className="mb-2">
-          Hello! I'm a Full Stack Developer based in Mississauga, Ontario, Canada.
-        </p>
-        <p>
-          I specialize in building web applications using React, Node.js, and various other technologies.
-        </p>
+      <p className="mb-2">
+        Hello! I'm a Full Stack Developer based in Mississauga, Ontario, Canada.
+      </p>
+      <p>
+        I specialize in building web applications using React, Node.js, and various other technologies.
+      </p>
       </motion.div>
     </div>
 
--- web-portfolio/src/components/Projects.tsx
+++ web-portfolio/src/components/Projects.tsx
@@ -1,13 +1,13 @@
-import { motion } from "motion/react";
+import { motion, type Variants } from "motion/react";
 import Project from "./Project";
 
 function Projects() {
   return (
     <motion.div
       className="w-3/4"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 40 }}
-      transition={{ duration: 0.5, delay: 0.6, ease: [0, 0, 0.4, 1] }}
+      initial="offscreen"
+      whileInView="onscreen"
+      variants={variants}
     >
       <div className="w-full flex flex-col justify-center">
         <h2 className="mb-4 text-xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl dark:text-white">
@@ -21,7 +21,7 @@ function Projects() {
           responsibilities={[
             "Developed a responsive front-end wedding website using React, ensuring cross-browser compatibility and smooth user interactions",
             "Implemented EmailJS for contact form functionality, validating message delivery and error handling to ensure reliability",
-            "eployed and managed the site with a Cloudflare Worker, testing performance, uptime, and security optimizations"
+            "Deployed and managed the site with a Cloudflare Worker, testing performance, uptime, and security optimizations"
           ]}
         />
       </div>
@@ -30,3 +30,17 @@ function Projects() {
 }
 
 export default Projects
+
+const variants: Variants = {
+  offscreen: {
+    y: 100
+  },
+  onscreen: {
+    y: 50,
+    transition: {
+      type: 'spring',
+      bounce: 0.4,
+      duration: 0.8
+    }
+  }
+}

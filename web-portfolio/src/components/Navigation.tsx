6f61476453d25773520b908dd91ca48515562898
Sungchul Park <spark0843@gmail.com>
2025-12-10 04:13:21 -0500

    lowercase button text

--- web-portfolio/src/components/Navigation.tsx
+++ web-portfolio/src/components/Navigation.tsx
@@ -36,7 +36,7 @@ function Link() {
 function Button() {
   return (
     <button className="bg-black text-white px-4 py-2.5 rounded-lg cursor-pointer">
-      Let's Work Together
+      Let's work together
     </button>
   )
 }

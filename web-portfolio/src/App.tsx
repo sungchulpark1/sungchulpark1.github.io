c1175cc6d94bb3594ff59df69b8c648a73d6e96a
Sungchul Park <spark0843@gmail.com>
2025-12-10 04:09:19 -0500

    add navbar

--- web-portfolio/src/App.tsx
+++ web-portfolio/src/App.tsx
@@ -1,22 +1,10 @@
-import Header from "./components/Header"
-import Experience from "./components/Experience"
-import Projects from "./components/Projects"
-import Education from "./components/Education"
-import Footer from "./components/Footer"
+import Navigation from "./components/Navigation"
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <Header />
-
-      <Experience />
-
-      <Projects />
-
-      <Education />
-
-      <Footer />
-    </div>
+    <>
+      <Navigation />
+    </>
   )
 }
 
---
+++ web-portfolio/src/components/Navigation.tsx
@@ -0,0 +1,52 @@
+function Content() {
+  return (
+    <div className="flex justify-between items-center w-250">
+      <Name />
+      <ExtraContent />
+    </div>
+
+  )
+}
+
+function Name() {
+  return (
+    <h1 className="font-black text-xl">
+      Sungchul Park
+    </h1>
+  )
+}
+
+function ExtraContent() {
+  return (
+    <div className="flex gap-8">
+      <Link />
+      <Button />
+    </div>
+  )
+}
+
+function Link() {
+  return (
+    <div className="flex items-center">
+      Case studies
+    </div>
+  )
+}
+
+function Button() {
+  return (
+    <button className="bg-black text-white px-4 py-2.5 rounded-lg cursor-pointer">
+      Let's Work Together
+    </button>
+  )
+}
+
+function Navigation() {
+  return (
+    <div className="flex justify-center h-20 px-6">
+      <Content />
+    </div>
+  )
+}
+
+export default Navigation
